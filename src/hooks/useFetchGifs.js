070a1e2805c82c28bc21ec5dd677469fe0119b5b
Katherina <katherina.lobos@gmail.com>
2023-05-03 15:54:13 -0400

    index.html actualizado

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -6,10 +6,12 @@ import { getGifs } from "../helpers/getGifs";
 export const useFetchGifs = (category) => {
 
   const [images, setImages] = useState([]);
-    
+  const [isLoading, setIsLoading] = useState(true);
+  
   const  getImages = async() => {
     const newImages = await getGifs(category);
     setImages(newImages);
+    setIsLoading(false);
   } 
       
   useEffect( () => {
